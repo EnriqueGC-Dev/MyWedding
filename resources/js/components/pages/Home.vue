b8f481269d1f579d20b7d925b18bd7a742255019
Kike <gc91.enrique@gmail.com>
2025-09-19 13:20:53 +0200

    Cambios esteticos

--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -4,22 +4,22 @@
          <div class="d-flex d-lg-none flex-column justify-center flex-grow-1">
             <img src="../../images/home-img.png" width="90%" alt="divider" class="mx-auto d-block">
             <span class="text-center">
-                    <h1 style="font-family: Mea Culpa; font-size: 8vh; color: #8B5CF6;">Laura y Kike</h1>  
-                </span>
-                <span class="text-center">
-                    <h2 style="font-family: Mea Culpa; font-size: 05vh; color: #A78BFA;">5 de Septiembre de 2026</h2>  
-                </span>
+                <h1 style="font-family: Mea Culpa; font-size: 8vh; color: #8B5CF6;">Laura y Kike</h1>  
+            </span>
+            <span class="text-center">
+                <h2 style="font-family: Mea Culpa; font-size: 05vh; color: #000;">29 de Agosto de 2026</h2>  
+            </span>
          </div>
-            <!-- Mostrar solo en desktop, centrado horizontalmente -->
-             <div class="d-none d-lg-flex flex-column align-items-center flex-grow-1">
-                 <img src="../../images/home-img.png" width="50%" alt="divider" class="mx-auto d-block">
-                <span class="text-center">
-                    <h1 style="font-family: Mea Culpa; font-size: 12vh; color: #8B5CF6;">Laura y Kike</h1>  
-                </span>
-                <span class="text-center">
-                    <h2 style="font-family: Mea Culpa; font-size: 07vh; color: #A78BFA;">5 de Septiembre de 2026</h2>  
-                </span>
-             </div>
+        <!-- Mostrar solo en desktop, centrado horizontalmente -->
+        <div class="d-none d-lg-flex flex-column align-items-center flex-grow-1">
+            <img src="../../images/home-img.png" width="50%" alt="divider" class="mx-auto d-block">
+            <span class="text-center">
+                <h1 style="font-family: Mea Culpa; font-size: 12vh; color: #8B5CF6;">Laura y Kike</h1>  
+            </span>
+            <span class="text-center">
+                <h2 style="font-family: Mea Culpa; font-size: 07vh; color: #A78BFA;">5 de Septiembre de 2026</h2>  
+            </span>
+        </div>
     </div>
 </template>
 
